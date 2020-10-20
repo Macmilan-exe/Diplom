18afbf9bc5ad6cb2caf4ab21ce64b920d1410403
misha <miha.martynenko.2002@gmail.com>
2020-10-20 09:57:25 +0300

    Added exception and map.

--- start3/src/by/gomel/ggmk/Main.java
+++ start3/src/by/gomel/ggmk/Main.java
@@ -1,25 +1,28 @@
 package by.gomel.ggmk;
 
 import by.gomel.ggmk.beans.Car;
+import by.gomel.ggmk.enums.WeeksDay;
 import by.gomel.ggmk.factories.CarFactory;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try(Scanner scanner = new Scanner(new FileReader(args[0]))) {
             List<Car> cars = new ArrayList<>();
+            Map<WeeksDay, Car> carMap = new EnumMap<>(WeeksDay.class);
             while (scanner.hasNextLine()){
-                Car car = CarFactory.getCarFromFactory(scanner.nextLine());
-                cars.add(car);
+                String [] argg = scanner.nextLine().split(";",2);
+                Car car = CarFactory.getCarFromFactory(argg[1]);
+                WeeksDay day = WeeksDay.valueOf(argg[0].toUpperCase());
+                carMap.put(day,car);
+                //map для хранения повторных значений
             }
-            for (Car car : cars) {
-                System.out.println(car);
+            for (Map.Entry<WeeksDay, Car> car:carMap.entrySet()) {
+                System.out.println(car.getKey() + "\t" + car.getValue());
             }
         }catch ( IOException e){
             System.err.println("Problem with file");
